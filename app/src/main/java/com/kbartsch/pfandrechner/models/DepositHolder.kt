0e2342d1cae58160aa744ef8037afd98d760df69
Kenneth Bartsch <k.b@fu-berlin.de>
2018-01-21 01:02:42 +0100

    changed method for the calculation of the total amount of money

--- app/src/main/java/com/kbartsch/pfandrechner/models/DepositHolder.kt
+++ app/src/main/java/com/kbartsch/pfandrechner/models/DepositHolder.kt
@@ -7,18 +7,18 @@ import android.view.View
 import android.widget.Button
 import android.widget.NumberPicker
 import com.kbartsch.pfandrechner.R
-import com.kbartsch.pfandrechner.totalMoney
 
 
 /**
  * Created by adunn on 7/5/17.
  */
-class DepositHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val nump: NumberPicker = itemView.findViewById<NumberPicker>(R.id.numberPicker)
-    private val button_add: Button = itemView.findViewById<Button>(R.id.button_add)
-    private val button_sub: Button = itemView.findViewById<Button>(R.id.button_sub)
-    private val itemTotal: TextView = itemView.findViewById<TextView>(R.id.ItemTextViewTotal)
-    private val depositImage: ImageView = itemView.findViewById<ImageView>(R.id.itemImageView)
+
+class DepositHolder(itemView: View, val totalItem: TextView, val deposits: ArrayList<Deposit>) : RecyclerView.ViewHolder(itemView) {
+    private val numberOfItem: NumberPicker = itemView.findViewById(R.id.numberPicker)
+    private val buttonAdd: Button = itemView.findViewById(R.id.button_add)
+    private val buttonSub: Button = itemView.findViewById(R.id.button_sub)
+    private val itemTotal: TextView = itemView.findViewById(R.id.ItemTextViewTotal)
+    private val depositImage: ImageView = itemView.findViewById(R.id.itemImageView)
     private val depositName: TextView = itemView.findViewById(R.id.ItemTextViewName)
     private val depositVal: TextView = itemView.findViewById(R.id.ItemTextViewPer)
 
@@ -27,36 +27,49 @@ class DepositHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         depositName.text = deposit.name
         depositVal.text = deposit.price.toString() + " €/Stk"
         itemTotal.text = "0.00 €"
-        nump.minValue = 0
-        nump.maxValue = 100
-        nump.wrapSelectorWheel = false
+        numberOfItem.minValue = 0
+        numberOfItem.maxValue = 100
+        numberOfItem.wrapSelectorWheel = false
 
-        nump.setOnValueChangedListener(NumberPicker.OnValueChangeListener { picker, oldVal, newVal ->
-            //Display the newly selected number from picker
-            nump.value = newVal
-            if (newVal >= oldVal) {
-                totalMoney += ((newVal - oldVal)*deposit.price).toFloat()
-
-            } else {
-                totalMoney -= ((oldVal - newVal)*deposit.price).toFloat()
+        fun update(Val: Int){
+            when (Val) {
+                -1   -> if (numberOfItem.value > 0) numberOfItem.value -= 1
+                1    -> numberOfItem.value += 1
+                else -> numberOfItem.value = Val
             }
-            itemTotal.text = "%.2f".format((nump.value * deposit.price)) + " €"
-            //TextTotal.text = "%.2f".format(totalMoney) + " €"
+            var totalMoney = 0.0
+            deposits[deposits.indexOf(deposit)].count = numberOfItem.value
+            deposits.forEach { element ->
+                totalMoney += element.count*element.price
+            }
+            itemTotal.text = "%.2f".format((numberOfItem.value * deposit.price)) + " €"
+            totalItem.text = "Gesamt " + "%.2f".format(totalMoney) + " €"
+
+        }
+
+        numberOfItem.setOnValueChangedListener( { _, _, newVal ->
+            update(newVal)
+            //Display the newly selected number from picker
+            //numberOfItem.value = newVal
+            //itemTotal.text = "%.2f".format((numberOfItem.value * deposit.price)) + " €"
+            //totalMoney = numberOfItem.value * deposit.price
+            //totalItem.text = "%.2f".format(numberOfItem.value * deposit.price) + " €"
         })
 
-        button_add.setOnClickListener {
-            nump.value += 1
-            totalMoney += deposit.price.toFloat()
-            itemTotal.text  = "%.2f".format((nump.value * deposit.price)) + " €"
-            //TextTotal.text      = "%.2f".format(totalMoney) + " €"
+        buttonAdd.setOnClickListener {
+            update(1)
+            //numberOfItem.value += 1
+            //totalMoney += numberOfItem.value * deposit.price
+            //itemTotal.text = "%.2f".format((numberOfItem.value * deposit.price)) + " €"
+            //totalItem.text = "%.2f".format(totalMoney) + " €"
         }
-        button_sub.setOnClickListener {
-            if (nump.value > 0) {
-                nump.value -= 1
-                totalMoney -= deposit.price.toFloat()
-                itemTotal.text = "%.2f".format((nump.value * deposit.price)) + " €"
-                //TextTotal.text      = "%.2f".format(totalMoney) + " €"
-            }
+        buttonSub.setOnClickListener {
+            update(-1)
+            //if (numberOfItem.value > 0) {
+            //    numberOfItem.value -= 1
+            //    totalMoney -= numberOfItem.value * deposit.price
+            //    itemTotal.text = "%.2f".format((numberOfItem.value * deposit.price)) + " €"
+            //    totalItem.text = "%.2f".format(totalMoney) + " €"
         }
     }
 }
